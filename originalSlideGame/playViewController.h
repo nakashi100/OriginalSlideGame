7b2689fa2172a6e97bba2226b9610353d3c6a41d
nakashi100 <tomohide.nakashima@gmail.com>
2015-01-25 18:53:55 +0800

    管理用の配列を作成

--- originalSlideGame/playViewController.h
+++ originalSlideGame/playViewController.h
@@ -11,7 +11,10 @@
 @interface playViewController : UIViewController
 - (IBAction)playButton:(id)sender;
 
+@property NSMutableArray *randNums;
 
+
+// ハコことなるview
 @property (weak, nonatomic) IBOutlet UIView *view1;
 @property (weak, nonatomic) IBOutlet UIView *view2;
 @property (weak, nonatomic) IBOutlet UIView *view3;
@@ -22,7 +25,7 @@
 @property (weak, nonatomic) IBOutlet UIView *view8;
 @property (weak, nonatomic) IBOutlet UIView *view9;
 
-
+// ハコに入れるimage (tag 11〜19)
 @property (weak, nonatomic) IBOutlet UIImageView *image1;
 @property (weak, nonatomic) IBOutlet UIImageView *image2;
 @property (weak, nonatomic) IBOutlet UIImageView *image3;
@@ -34,10 +37,13 @@
 @property (weak, nonatomic) IBOutlet UIImageView *image9;
 
 
+// 見本用 (tag 21〜29)
 @property (weak, nonatomic) IBOutlet UIImageView *mihon9;
 
 
-@property NSMutableArray *randNums;
+// tap判定用の透明view (tag 1〜9)
+
+
 
 
 @end
--- originalSlideGame/playViewController.m
+++ originalSlideGame/playViewController.m
@@ -33,8 +33,7 @@
     self.mihon9.image = [UIImage imageNamed:@"mihonSample"];
     
     
-    
-    // for文でループ処理を使いたいが、、、
+    // for文でループ処理を使いたいがうまくできない、、、
 //    for (int i=1; i<10; i++) {
 //        NSString *imageNum = [NSString stringWithFormat:@"image%d", i];
 //        NSString *sampleNum = [NSString stringWithFormat:@"sample%d", i];
@@ -51,7 +50,7 @@
 - (IBAction)playButton:(id)sender {
     
     // image9(tag:9)を削除する
-     [[self.view viewWithTag:9] removeFromSuperview];
+     [[self.view viewWithTag:19] removeFromSuperview];
     
     
     // image1〜8を並び替える
@@ -66,7 +65,7 @@
     NSString *randPic7 = [NSString stringWithFormat:@"sample%@", self.randNums[6]];
     NSString *randPic8 = [NSString stringWithFormat:@"sample%@", self.randNums[7]];
     
-    self.image1.image = [UIImage imageNamed:randPic1];  //応用するためにはtagでセットしたほうがよさそう
+    self.image1.image = [UIImage imageNamed:randPic1];
     self.image2.image = [UIImage imageNamed:randPic2];
     self.image3.image = [UIImage imageNamed:randPic3];
     self.image4.image = [UIImage imageNamed:randPic4];
@@ -74,11 +73,27 @@
     self.image6.image = [UIImage imageNamed:randPic6];
     self.image7.image = [UIImage imageNamed:randPic7];
     self.image8.image = [UIImage imageNamed:randPic8];
+
     
     
-//    if ([self.view viewWithTag:9] == nil) {
-//        NSLog(@"空です");
+    // ゲーム管理用の配列を作成する
+    // array[0]は空判定(0が空で1が空でない)、array[1]はtabの番号、array[2]は実際の値(パズル完成の判定に用いる))
+    NSMutableArray *viewArray1 = [@[@1,@11,self.randNums[0]]mutableCopy];
+    NSMutableArray *viewArray2 = [@[@1,@12,self.randNums[1]]mutableCopy];
+    NSMutableArray *viewArray3 = [@[@1,@13,self.randNums[2]]mutableCopy];
+    NSMutableArray *viewArray4 = [@[@1,@14,self.randNums[3]]mutableCopy];
+    NSMutableArray *viewArray5 = [@[@1,@15,self.randNums[4]]mutableCopy];
+    NSMutableArray *viewArray6 = [@[@1,@16,self.randNums[5]]mutableCopy];
+    NSMutableArray *viewArray7 = [@[@1,@17,self.randNums[6]]mutableCopy];
+    NSMutableArray *viewArray8 = [@[@1,@18,self.randNums[7]]mutableCopy];
+    NSMutableArray *viewArray9 = [@[@0,@0,@0]mutableCopy];     //view9が最初に空になるので配列には{0,0}を入れておく
+
+    //うまくできない
+//    for (int i=1; i<10; i++) {
+//        NSString *testArray = [NSString stringWithFormat:@"viewArray%d",i];
+//        NSLog(@"%@",testArray);
 //    }
+
 }
 
 
@@ -106,64 +121,64 @@
 }
 
 
-/***********************************************************
-                    スライドした時の処理
-***********************************************************/
+/****************************************************************
+                        スライドした時の処理
+****************************************************************/
 
 - (void)touchesBegan:(NSSet *)touches withEvent:(UIEvent *)event
 {
     UITouch *touch = [touches anyObject];
+
+    NSLog(@"タッチしたビューは、%@", touch.view);
+    NSLog(@"image1は、%@", self.image1.tag);
     
     
-    switch (touch.view.tag) {
-        case 8:
-            if ([self.view viewWithTag:5] == nil) {
-                //
-            }else if ([self.view viewWithTag:7] == nil) {
-                //
-            }else if ([self.view viewWithTag:9] == nil) {
-                NSLog(@"成功です");
-                [self push];
+    
+    
+    
+    
+    
+    
+//    switch (touch.view.tag) {
+//        case 8:
+//             NSLog(@"やーやーやー");
+//            if ([self.view viewWithTag:5] == nil) {
+//                //
+//            }else if ([self.view viewWithTag:7] == nil) {
+//                //
+//            }else if ([self.view viewWithTag:9] == nil) {
+//                NSLog(@"成功です");
+//            if ([self.view viewWithTag:9] == nil) {
+//
+//                [self push];
 //                self.image8.frame = CGRectMake(100, 0, self.image8.frame.size.width, self.image8.frame.size.height);
-            }
-            break;
-            
-        default:
-            break;
-    }
-    
-    
-//    NSLog(@"%@",[self.view viewWithTag:100]);
-    
-//    if ([self.view viewWithTag:9] == nil) {
-//        NSLog(@"二度目成功です");
+//            }
+//            break;
+//
+//        default:
+//            break;
 //    }
-//    
-//    if ([self.view viewWithTag:8] == nil) {
-//        NSLog(@"三度目成功です");
-//    }
+//
 
-    NSLog(@"%@",[self.view viewWithTag:8]);
-    NSLog(@"%@",[self.view viewWithTag:9]);
     
 }
 
 
 
 
-
-- (IBAction)push{
-    [UIView animateWithDuration:0.1f
-                          delay:0.0f
-                        options:UIViewAnimationOptionCurveEaseIn
-                     animations:^{
-                         CGRect rect = CGRectMake([self.view viewWithTag:8].frame.size.width+2, 0, [self.view viewWithTag:8].frame.size.width, [self.view viewWithTag:8].frame.size.height);
-                         [[self.view viewWithTag:8] setFrame:rect];
-                     }
-                     completion:^(BOOL finished){
+//
+//- (IBAction)push{
+//    [UIView animateWithDuration:0.1f
+//                          delay:0.0f
+//                        options:UIViewAnimationOptionCurveEaseIn
+//                     animations:^{
+//                         CGRect rect = CGRectMake([self.view viewWithTag:8].frame.size.width+2, 0, [self.view viewWithTag:8].frame.size.width, [self.view viewWithTag:8].frame.size.height);
+//                         [[self.view viewWithTag:8] setFrame:rect];
+//                     }
+//                     completion:^(BOOL finished){
 //                         [[self.view viewWithTag:8] setBackgroundColor:[UIColor redColor]];
-                    }
-     ];
-}
+//                    }
+//     ];
+//}
 
 @end
